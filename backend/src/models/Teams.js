376aab664897a18c1637fe0674f3b0ac1fcba590
Felipe Lima <felipe.pereira9012@gmail.com>
2026-05-09 17:50:39 -0300

    feat(models): cria model de teams

---
+++ backend/src/models/Teams.js
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import connection from "../database";
+
+const Teams = connection.define('teams', {
+    id: {
+        type: DataTypes.UUID,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+})
